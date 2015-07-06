21c69be354bb8ed4570e5889d738143de5e62ae8
Aleksandr Marchenko <marrchenko@gmail.com>
2015-07-06 20:38:34 +0300

    project structure

--- app/src/main/java/com/stenbergroom/materialdrawerapp/app/MainActivity.java
+++ app/src/main/java/com/stenbergroom/materialdrawerapp/app/MainActivity.java
@@ -2,15 +2,23 @@ package com.stenbergroom.materialdrawerapp.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        if(toolbar != null){
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
