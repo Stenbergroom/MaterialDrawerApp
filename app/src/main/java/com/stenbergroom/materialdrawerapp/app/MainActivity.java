e78b9ef600b1cdab7642be866c2d5e84af6ee243
Aleksandr Marchenko <marrchenko@gmail.com>
2015-07-06 20:39:46 +0300

    project structure

--- app/src/main/java/com/stenbergroom/materialdrawerapp/app/MainActivity.java
+++ app/src/main/java/com/stenbergroom/materialdrawerapp/app/MainActivity.java
@@ -1,11 +1,8 @@
 package com.stenbergroom.materialdrawerapp.app;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
